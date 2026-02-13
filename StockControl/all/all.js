91e5355ef9276b4cb73716523d707be0aa5196a3
Flavia Melo <flay.melo5@gmail.com>
2026-02-13 13:33:31 -0300

    newTable loop modified

--- StockControl/all/all.js
+++ StockControl/all/all.js
@@ -23,7 +23,7 @@ const proxyURL = 'https://cors-anywhere.herokuapp.com/';
 
 async function fetchSheetData() {
     const response = await fetch(apiURL)
-    const data = await response.json() // await response.text()
+    const data = await response.json() //const text = await response.text()
     // const json = JSON.parse(text.substring(47).slice(0, -2))
 
     // const data = parse(json)
@@ -41,10 +41,10 @@ async function fetchSheetData() {
             newTable.innerHTML =  `
                 <table>
                         <tr>
-                            <td class="barcode"><p>${data[i]['Codigo']}</p></td>
-                            <td class="product-name">${data[i]['Nome']}</td>
-                            <td class="price"><span>R$</span>${data[i]['Preco']}</td>
-                            <td class="quantity">${data[i]['Quantidade']}</td>
+                            <td class="barcode"><p>${produto.Codigo}</p></td>
+                            <td class="product-name">${produto.Nome}</td>
+                            <td class="price"><span>R$</span>${produto.Preco}</td>
+                            <td class="quantity">${produto.Quantidade}</td>
                             <td class="edit">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/></svg>
                             </td>
